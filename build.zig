a5161606f48f47892810fc2578a0cbb00cb088ac
Lucas Elisei <lelisei@tandemdiabetes.com>
2025-01-23 13:23:29 +0100

    Modify build.zig to build every program in 'days' folder

--- build.zig
+++ build.zig
@@ -1,66 +1,49 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+fn lessThan(_: void, lhs: []const u8, rhs: []const u8) bool {
+    return std.ascii.lessThanIgnoreCase(lhs, rhs);
+}
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const day01 = b.addExecutable(.{
-        .name = "01-historian-hysteria",
-        .root_source_file = b.path("days/01.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // Get all zig files in 'days' directory
+    const cwd = std.fs.cwd();
+    var days_dir = try cwd.openDir("days", .{ .iterate = true, .access_sub_paths = false });
+    defer days_dir.close();
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(day01);
+    // Store files in an intermediate array because we want to sort them alphabetically
+    var files = std.ArrayList([]const u8).init(b.allocator);
+    defer files.deinit();
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    // const run_cmd = b.addRunArtifact(day01);
+    var iter = days_dir.iterate();
+    while (try iter.next()) |entry| {
+        if ((entry.kind != .file) or (std.ascii.endsWithIgnoreCase(entry.name, ".zig") == false)) {
+            continue;
+        }
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    // run_cmd.step.dependOn(b.getInstallStep());
+        try files.append(entry.name);
+    }
 
-    // // This allows the user to pass arguments to the application in the build
-    // // command itself, like this: `zig build run -- arg1 arg2 etc`
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
+    std.mem.sort([]const u8, files.items, {}, lessThan);
 
-    // // This creates a build step. It will be visible in the `zig build --help` menu,
-    // // and can be selected like this: `zig build run`
-    // // This will evaluate the `run` step rather than the default, which is "install".
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
+    for (files.items) |file| {
+        std.debug.print("Building '{s}'...\n", .{file});
 
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
+        var path = std.ArrayList(u8).init(b.allocator);
+        defer path.deinit();
 
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        try path.appendSlice("days/");
+        try path.appendSlice(file);
 
-    // // Similar to creating the run step earlier, this exposes a `test` step to
-    // // the `zig build --help` menu, providing a way for the user to request
-    // // running the unit tests.
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_exe_unit_tests.step);
+        const exe = b.addExecutable(.{
+            .name = file[0..(std.ascii.indexOfIgnoreCase(file, ".zig") orelse unreachable)],
+            .root_source_file = b.path(path.items),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        b.installArtifact(exe);
+    }
 }
